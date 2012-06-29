dbcf7d6451ec2b2d83b7f780170c1a5e3497fe4c
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2012-06-30 04:13:16 +0900

    Add tests.

--- tests/Esperance/Tests/AssertionTest.php
+++ tests/Esperance/Tests/AssertionTest.php
@@ -22,4 +22,21 @@ class AssertionTest extends TestCase
     {
         $this->expect(1)->to->be(2);
     }
+
+    /**
+     * @test
+     */
+    public function not_be_should_be_ok_if_subject_is_not_equal_to_object()
+    {
+        $this->expect(1)->to->not->be(2);
+    }
+
+    /**
+     * @test
+     * @expectedException \Esperance\Error
+     */
+    public function not_be_should_be_error_if_subject_is_equal_to_object()
+    {
+        $this->expect(1)->to->not->be(1);
+    }
 }
